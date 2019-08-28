8f20555d2bf3613cfdf46e44a6c6c348742f54df
laercio souza <laercio-phbhotmail.com>
2019-08-28 10:25:08 -0300

    ..

---
+++ js/estacionamento/estacionamento.js
@@ -0,0 +1,46 @@
+function modificar(){
+
+    document.getElementById('img').src = "imagens/azul.png";
+
+}
+
+function alterar(){
+
+    document.getElementById('imagem').src = "imagens/vermelho.png";
+}
+
+function mudar(){
+    document.getElementById('foto').src = "imagens/azul.png";
+}
+
+function modificar2(){
+
+    document.getElementById('foto2').src = "imagens/vermelho.png";
+}
+
+function mudar2(){
+
+    document.getElementById('foto3').src = "imagens/azul.png";
+
+}
+
+function alterar2(){
+
+    document.getElementById('imagem2').src = "imagens/vermelho.png";
+}
+
+function mudar3(){
+
+    document.getElementById('foto4').src = "imagens/vermelho.png";
+}
+
+function mudar4(){
+
+    document.getElementById('foto5').src = "imagens/azul.png";
+
+}
+
+function mudar5(){
+
+    document.getElementById('foto6').src = "imagens/vermelho.png";
+}
