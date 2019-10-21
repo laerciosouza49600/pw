a6b1ede3bff128f2cee87e117dfb3b126cb18d97
Laercio Souza da Silva <20161TIMINF013@ifpi.int>
2019-10-21 12:48:01 -0200

    ..

--- php/diretorio/teste.php
+++ php/diretorio/teste.php
@@ -5,15 +5,16 @@
 	<input type = "radio" name = "calculadora" value = "soma"> soma
 	<input type = "radio" name = "calculadora" value = "sub" > subtracao
 	<input type = "radio" name = "calculadora" value = "produto"> multiplicacao
-	<input type = "radio" name = "calculadora" value = "divisao"> divisao
-	<input type = "submit">
+	<input type = "radio" name = "calculadora" value = "divisao"> divisao </br>
+	<input type = "number" name = "a"> </br>
+	<input type = "number" name = "b"> </br>
+	<button type = "submit"> calculo </button>
 </form>
-
 
 <?php
 	
-	$a = 4;
-	$b = 5;
+	$a = $_POST["a"];
+	$b = $_POST["b"];
 	$c;
 
 	
@@ -23,6 +24,28 @@
 
 		echo "soma == ". $c;
 
+	}else if($_POST['calculadora'] == "sub"){
+
+		$c = $a-$b;
+
+		echo "subtração == ".$c;
+
+	}else if($_POST['calculadora'] == "produto"){
+
+		$c = $a*$b;
+
+		echo "<h3>multiplicação == ". $c. "</h3>";
+
+	}else if($_POST['calculadora'] == "divisao"){
+
+		$c = $a/$b;
+
+		echo "divisão == ". $c;
+	}else{
+
+		echo " <h1 align = 'center'> nenhum item selecionado". "</h1>";
 	}
+
+	
 
 ?>
